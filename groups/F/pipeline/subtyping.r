ffe8e9309a24c4c9179e227f947eacca4c95afac
Andreas Schlicker <a.schlicker@nki.nl>
2013-09-30 16:36:47 +0200

    Small changes

--- groups/F/pipeline/subtyping.r
+++ groups/F/pipeline/subtyping.r
@@ -8,16 +8,21 @@ library(synapseClient)
 library(rGithubClient)
 
 crcRepo = getRepo("andreas-schlicker/crcsc")
-iNMFSignatures = getPermlink(crcRepo, "groups/F/pipeline/inmf_signatures.rdata")
-load(iNMFSignatures)
+
 sourceRepoFile(crcRepo, "groups/F/pipeline/inmf.r")
 iNMFFunctions = getPermlink(crcRepo, "groups/F/pipeline/inmf.r")
 sourceRepoFile(crcRepo, "groups/F/pipeline/synapse_helper.r")
 helperFunctions = getPermlink(crcRepo, "groups/F/pipeline/synapse_helper.r")
+sourceRepoFile(crcRepo, "groups/F/pipeline/plotting.r")
+plottingFunctions = getPermlink(crcRepo, "groups/F/pipeline/plotting.r")
+
 thisScript = getPermlink(crcRepo, "groups/F/pipeline/subtyping.r")
 
 # password will be request after calling this
 synapseLogin("a.schlicker@nki.nl")
+
+iNMFSignaturesSynId = "syn2245383"
+iNMFSignatures = read.table(getFileLocation(synGet(iNMFSignaturesSynId)), header=TRUE, sep="\t", quote="", stringsAsFactors=FALSE)
 
 coreExprList = list(
 		agendia_gse42284=list(synId="syn2192792", sigId="symbol", mapSynId="syn2192791", mapId="symbol", file="GSE42284_normalized_data_matrix.txt", is.logr=TRUE),
@@ -31,60 +36,82 @@ coreExprList = list(
 		tcga_rnaseq=list(synId="syn2161141", sigId="symbol", is.logr=FALSE),
 		mdanderson=list(synId="syn2233387", sigId="symbol", mapSynId="syn2233216", mapId="GeneName", is.logr=TRUE))
 
-coreResults = lapply(coreExprList, function(x) {
-			sep = "\t"
-			if (!is.null(x$sep)) {
-				sep = x$sep
-			}
-			
-			quote = ""
-			if (!is.null(x$quote)) {
-				quote = x$quote
-			}
-			
-			# Get the expression data
-			file = ""
-			if (!is.null(x$file))
-				file = x$file
-			data.mat = loadMatrix(x$synId, file=file, sep=sep, quote=quote)
-			
-			if (!x$is.logr) {
-				data.mat = data.mat - apply(data.mat, 1, mean)
-			}
-			
-			sig.id = "ps"
-			if (!is.null(x$sigId)) {
-				sig.id = x$sigId
-			}
-			
-			mapping = NULL
-			if (!is.null(x$mapSynId)) {
-				# Get the mapping from data set IDs to signature IDs
-				map.mat = loadMatrix(x$mapSynId)
-				mapping = getMapping(map.mat, x$mapId)
-			}
-			# Get the iNMF signatures
-			signatures = signaturesList(rownames(data.mat), sig.id, mapping)
-			
-			# Run bootstrapped iNMF subtyping
-			bootstrappediNMF(data.mat, signatures, runs=10, procCores=10)
-		})
-
-
 publicExprList = list(
-		gse10961=toEntrez("syn2177194","syn2177195"),
-		gse13067=toEntrez("syn2177888","syn2177889"),
-		gse13294=toEntrez("syn2177894","syn2177895"),
-		gse14333=toEntrez("syn2181079","syn2181006"),
-		gse15960=toEntrez("syn2177199","syn2177200"),
-		gse17537=toEntrez("syn2178128","syn2178129"),
-		gse20916=toEntrez("syn2177899","syn2177898"),
-		gse2109=toEntrez("syn2177169","syn2177168"),
-		gse23878=toEntrez("syn2177902","syn2178063"),
-		gse37892=toEntrez("syn2178082","syn2178089"),
-		gse4107=toEntrez("syn2177179","syn2177180"),
-		gse4183=toEntrez("syn2177187","syn2177188"),
-		gse8671=toEntrez("syn2181088","syn2181090"))
+		gse10961=list(synId="syn2177194", is.logr=FALSE, annSynId="syn2177195"),
+		gse13067=list(synId="syn2177888", is.logr=FALSE, annSynId="syn2177889"),
+		gse13294=list(synId="syn2177894", is.logr=FALSE, annSynId="syn2177895"),
+		gse14333=list(synId="syn2181079", is.logr=FALSE, annSynId="syn2181006"),
+		gse15960=list(synId="syn2177199", is.logr=FALSE, annSynId="syn2177200"),
+		gse17537=list(synId="syn2178128", is.logr=FALSE, annSynId="syn2178129"),
+		gse20916=list(synId="syn2177899", is.logr=FALSE, annSynId="syn2177898"),
+		gse2109=list(synId="syn2177169", is.logr=FALSE, annSynId="syn2177168"),
+		gse23878=list(synId="syn2177902", is.logr=FALSE, annSynId="syn2178063"),
+		gse37892=list(synId="syn2178082", is.logr=FALSE, annSynId="syn2178089"),
+		gse4107=list(synId="syn2177179", is.logr=FALSE, annSynId="syn2177180"),
+		gse4183=list(synId="syn2177187", is.logr=FALSE, annSynId="syn2177188"),
+		gse8671=list(synId="syn2181088", is.logr=FALSE, annSynId="syn2181090"))
 
-allData = c(coreExprList,publicExprList)
+
+allResults = list()
+for (n in c(names(coreExprList), names(publicExprList()))) {
+	x = coreExprList[[n]]
+	
+	sep = "\t"
+	if (!is.null(x$sep)) {
+		sep = x$sep
+	}
+	
+	quote = ""
+	if (!is.null(x$quote)) {
+		quote = x$quote
+	}
+	
+	# Get the expression data
+	file = ""
+	if (!is.null(x$file)) {
+		file = x$file
+	}
+	data.mat = loadMatrix(x$synId, file=file, sep=sep, quote=quote)
+	
+	if (!x$is.logr) {
+		data.mat = data.mat - apply(data.mat, 1, mean)
+	}
+	
+	sig.id = "ps"
+	if (!is.null(x$sigId)) {
+		sig.id = x$sigId
+	}
+	
+	mapping = NULL
+	if (!is.null(x$mapSynId)) {
+		# Get the mapping from data set IDs to signature IDs
+		map.mat = loadMatrix(x$mapSynId)
+		mapping = getMapping(map.mat, x$mapId)
+	}
+	# Get the iNMF signatures
+	signatures = signaturesList(rownames(data.mat), sig.id, iNMFSignatures, mapping)
+	
+	# Run bootstrapped iNMF subtyping
+	coreResults[[n]] = bootstrappediNMF(data.mat, signatures, runs=1, procCores=1)
+}
+
+plotting.cols = c("gray10", "gray40", "gray70", "springgreen4", "springgreen2")
+plotting.shapes = 15:19
+for (x in names(allResults)) {
+	png(paste(x, "probability.png", sep="_"), width=6000, height=3000, res=300)
+	print(probabilityPlot(allResults[[x]][[1]], colors=plotting.cols))
+	dev.off()
+	
+	png(paste(x, "probability_faceted.png", sep="_"), width=6000, height=3000, res=300)
+	print(facetedProbabilityPlot(allResults[[x]][[1]], colors=plotting.cols))
+	dev.off()
+	
+	png(paste(x, "margins.png", sep="_"), width=6000, height=3000, res=300)
+	print(marginPlot(allResults[[x]][[1]], colors=plotting.cols, shapes=plotting.shapes))
+	dev.off()
+	
+	png(paste(x, "cooccurrence.png", sep="_"), width=3000, height=3000, res=300)
+	coclusteringPlot(allResults[[x]][[2]])
+	dev.off()
+}
 
