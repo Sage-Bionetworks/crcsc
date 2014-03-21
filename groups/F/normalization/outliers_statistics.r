f388c918b8d5bec57e571cb5fcb33bfdefa6af4f
Andreas Schlicker <a.schlicker@nki.nl>
2014-03-21 13:27:20 +0100

    Changes error checking when storing data in Synapse

--- groups/F/normalization/outliers_statistics.r
+++ groups/F/normalization/outliers_statistics.r
@@ -84,7 +84,7 @@ for (i in 1:nrow(allData)) {
 				while (failed && (tries < 5)) {
 					res = tryCatch(synStore(synFile, used=resources),
 							error=function(e) NA)
-					if (!is.na(res)) {
+					if (class(res) == "File") {
 						failed=FALSE
 					}
 					tries = tries + 1
